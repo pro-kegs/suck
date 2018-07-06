2ddbb7d1b92ec7f5419e539e8189943f66744e26
Kelvin Sherlock <ksherlock@gmail.com>
2018-07-05 20:32:15 -0400

    remove unused strnstr prototype since it causes warnings on OS X

--- killfile.c
+++ killfile.c
@@ -55,7 +55,6 @@ int check_a_group(PMaster, POneKill, char *, char **);
 void print_debug(PKillStruct, const char *);
 void debug_one_kill(POneKill);
 void add_to_linkedlist(pmy_regex *, pmy_regex);
-const char *strnstr(const char *, const char *);
 pmy_regex regex_scan(char *, char, int, int, char);
 int regex_check(char *, pmy_regex, int);
 
